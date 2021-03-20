c2a15cf4c0e88cd3fcc819b6cb916e3894e5ee0f
Dascal Tiberiu <tiberiu.purcel@gmail.com>
2021-03-20 16:15:47 +0200

    Up Model Forms (2:49:01)

--- leads/views.py
+++ leads/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Lead, Agent
+from .forms import LeadForm
 
-# Create your views here.
+def lead_list(request):
+    leads = Lead.objects.all()
+    context = {
+        "leads": leads
+    }
+    return render(request, "leads/lead_list.html", context)
+
+
+def lead_detail(request, pk):
+    lead = Lead.objects.get(id=pk)
+    context = {
+        "lead": lead
+    }
+    return render(request, "leads/lead_detail.html", context)
+
+
+def lead_create(request):
+    form = LeadForm()
+    if request.method == "POST":
+        print('Receiving a post request')
+        form = LeadForm(request.POST)
+        if form.is_valid():
+            print('Form is valid')
+            print(form.cleaned_data)
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            age = form.cleaned_data['age']
+            agent = Agent.objects.first()
+
+            Lead.objects.create(
+                first_name=first_name,
+                last_name=last_name,
+                age=age,
+                agent=agent
+            )
+
+            return redirect("/leads")
+    context = {
+        "form": LeadForm()
+    }
+    
+    return render(request, "leads/lead_create.html", context)
