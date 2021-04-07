f408969c9ac98fd36760249ea504e3ff027cb803
Dascal Tiberiu <tiberiu.purcel@gmail.com>
2021-04-08 00:26:51 +0300

    Up to Filter Agents (5:28:31)

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'leads'
+    'leads',
+    'agents'
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,4 @@ STATIC_ROOT = "static_root"
 AUTH_USER_MODEL = 'leads.User'
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 LOGIN_REDIRECT_URL = "/leads"
+LOGIN_URL = "/login"
